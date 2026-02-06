8ca8e0aee2a7f34dbb9bb6b1aa59e3b77b93e541
Rafael <1221267@isep.ipp.pt>
2026-02-06 21:04:26 +0000

    inicial layout

---
+++ FrontendConverter/src/components/MainContent/MainContent.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './MainContent.css';
+
+const MainContent: React.FC = () => {
+  return (
+    <main className="mainContent">
+      <h1>Conteúdo Principal</h1>
+      <p>Aqui vai o conteúdo do site...</p>
+            <p>Aqui vai o conteúdo do site...</p>
+      <p>Aqui vai o conteúdo do site...</p>
+      <p>Aqui vai o conteúdo do site...</p>
+
+      <p>Aqui vai o conteúdo do site...</p>
+
+      <p>Aqui vai o conteúdo do site...</p>
+
+      <p>Aqui vai o conteúdo do site...</p>
+
+      <p>Aqui vai o conteúdo do site...</p>
+      <p>Aqui vai o conteúdo do site...</p>
+      <p>Aqui vai o conteúdo do site...</p>
+      <p>Aqui vai o conteúdo do site...</p>
+      <p>Aqui vai o conteúdo do site...</p>
+      <p>Aqui vai o conteúdo do site...</p>
+      <p>Aqui vai o conteúdo do site...</p>
+      <p>Aqui vai o conteúdo do site...</p>
+    
+      <p>Aqui vai o conteúdo do site...</p>
+
+      <p>Aqui vai o conteúdo do site...</p>
+
+      <p>Aqui vai o conteúdo do site...</p>
+
+      <p>Aqui vai o conteúdo do site...</p>
+
+      <p>Aqui vai o conteúdo do site...</p>
+
+      <p>Aqui vai o conteúdo do site...</p>
+
+
+    </main>
+  );
+};
+
+export default MainContent;
---
+++ FrontendConverter/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar: React.FC = () => {
+  return (
+    <aside className="sidebar">
+      <h2>Anúncios</h2>
+      <p>Espaço reservado para anúncios.</p>
+    </aside>
+  );
+};
+
+export default Sidebar;
